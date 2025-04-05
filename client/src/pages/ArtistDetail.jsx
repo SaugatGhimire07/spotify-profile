c654a21e578e3f135adec5e1e7545b397a3f25c2
SaugatGhimire07 <Ghimiresaugat987@gmail.com>
2025-04-04 22:05:27 -0400

    Update ArtistDetail layout for improved responsiveness on larger screens

--- client/src/pages/ArtistDetail.jsx
+++ client/src/pages/ArtistDetail.jsx
@@ -35,7 +35,7 @@ function ArtistDetail({ token }) {
   return (
     <div className="flex flex-col md:flex-row">
       <Sidebar />
-      <div className="w-full md:flex-1 md:ml-[100px]">
+      <div className="w-full md:flex-1 lg:ml-[100px]">
         <div className="bg-[#181818] p-[20px] md:py-[40px] md:px-[30px] lg:p-[80px] max-w-[1400px] mx-auto">
           {firstLoad ? (
             <div className="flex items-center justify-center min-h-[400px]">
