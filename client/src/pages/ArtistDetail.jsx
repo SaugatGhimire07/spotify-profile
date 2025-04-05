1729f176257906e861d55d97c88fa191e8c5d724
SaugatGhimire07 <Ghimiresaugat987@gmail.com>
2025-04-04 22:13:06 -0400

    Add min-height to ArtistDetail container for improved layout consistency

--- client/src/pages/ArtistDetail.jsx
+++ client/src/pages/ArtistDetail.jsx
@@ -36,7 +36,7 @@ function ArtistDetail({ token }) {
     <div className="flex flex-col md:flex-row">
       <Sidebar />
       <div className="w-full md:flex-1 lg:ml-[100px]">
-        <div className="bg-[#181818] p-[20px] md:py-[40px] md:px-[30px] lg:p-[80px] max-w-[1400px] mx-auto">
+        <div className="bg-[#181818] p-[20px] md:py-[40px] md:px-[30px] lg:p-[80px] max-w-[1400px] mx-auto min-h-screen	">
           {firstLoad ? (
             <div className="flex items-center justify-center min-h-[400px]">
               <Loading />
